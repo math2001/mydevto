ba16a566e659b6eb4e5d54e0e88e4f84e086af7c
Mathieu PATUREL <australie.p@gmail.com>
2018-09-19 17:13:05 +1000

    feat(login): add login button on page

--- web/static/app.js
+++ web/static/app.js
@@ -1,1 +1,9 @@
-alert("hello world!")
+window.addEventListener("load", e => {
+
+  document.querySelector("#login").addEventListener("click", e => {
+    e.preventDefault()
+    const win = window.open(e.target.href, 'Login with github', 'width=500,height=500,centerscreen')
+    if (window.focus) { win.focus() }
+  })
+
+})
