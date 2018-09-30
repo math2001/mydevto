18dfd72041fcd5f970aa59cd4a4d63093c70bace
Mathieu PATUREL <australie.p@gmail.com>
2018-09-30 10:10:45 +1000

    feat(resp): automatically handle errors

--- resp/resp.go
+++ resp/resp.go
@@ -4,14 +4,15 @@ package resp
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
 
 // Message writes a response in the form { "type": <str>, "message": <str> }
 // It is mainly a util function for the Error and Success functions
-func Message(w http.ResponseWriter, r *http.Request, code int, kind string, format string, a ...interface{}) error {
+func Message(w http.ResponseWriter, r *http.Request, code int, kind string, format string, a ...interface{}) {
 	w.WriteHeader(code)
-	return Encode(w, r, map[string]string{
+	Encode(w, r, map[string]string{
 		"type":    kind,
 		"message": fmt.Sprintf(format, a...),
 	})
@@ -20,30 +21,38 @@ func Message(w http.ResponseWriter, r *http.Request, code int, kind string, form
 // Error writes a message of type "error" with "message" msg. It is typically
 // used to give more information about an error to the user. This is when the
 // information isn't sensitive, otherwise InternalError is used.
-func Error(w http.ResponseWriter, r *http.Request, code int, format string, a ...interface{}) error {
-	return Message(w, r, code, "error", format, a...)
+func Error(w http.ResponseWriter, r *http.Request, code int, format string, a ...interface{}) {
+	Message(w, r, code, "error", format, a...)
 }
 
 // Success writes a message of type "success" with "message" msg. It is
 // typically used to confirm that a post request has been successful
-func Success(w http.ResponseWriter, r *http.Request, format string, a ...interface{}) error {
-	return Message(w, r, http.StatusOK, "success", format, a...)
+func Success(w http.ResponseWriter, r *http.Request, format string, a ...interface{}) {
+	Message(w, r, http.StatusOK, "success", format, a...)
 }
 
 // InternalError writes a static object:
 // {"kind": "error", "message": "Internal error"}
 // This is used a lot, when the error that caused the failure could be
 // sensitive, and we don't want to give too much information
-func InternalError(w http.ResponseWriter, r *http.Request) error {
-	return Error(w, r, http.StatusInternalServerError, "Internal error")
+func InternalError(w http.ResponseWriter, r *http.Request) {
+	Error(w, r, http.StatusInternalServerError, "Internal error")
 }
 
 // Encode writes the object to the page, formatting according the User-Agent
-func Encode(w http.ResponseWriter, r *http.Request, e interface{}) error {
+func Encode(w http.ResponseWriter, r *http.Request, e interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
 	if r.UserAgent() != "js" {
 		enc.SetIndent("", "  ")
 	}
-	return enc.Encode(e)
+	if err := enc.Encode(e); err != nil {
+		log.Printf("Errored encoding object to ResponseWriter (writing manually): %s", err)
+		// manually print out some JSON
+		_, err = fmt.Fprintf(w, `{ "type": "error", "message": "Internal error" }`)
+		if err != nil {
+			// there's nothing we can do now...
+			log.Printf("Errored writing to ResponseWriter: %s", err)
+		}
+	}
 }
