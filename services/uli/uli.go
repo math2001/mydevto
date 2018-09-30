a6561cd05acf8c76d4b840adcda84f3b254a82ed
Mathieu PATUREL <australie.p@gmail.com>
2018-09-30 14:44:38 +1000

    feat(uli): add logger

---
+++ services/uli/uli.go
@@ -0,0 +1,54 @@
+// Package uli provides an interface that automatically logs the request's
+// identity as a prefix
+package uli
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"time"
+)
+
+const logsdir = "./logs"
+
+var logger *log.Logger
+
+// Init inits the logger
+func Init(version string) {
+	f := createfile(version)
+	logger = log.New(f, "", log.LstdFlags)
+}
+
+// createdirs makes sure all the directory where the logs are going to be
+// written exist
+func createfile(version string) io.Writer {
+	// creates logsdir if it doesn't exist
+	if _, err := os.Stat(logsdir); os.IsNotExist(err) {
+		if err := os.Mkdir(logsdir, 0755); err != nil {
+			log.Fatalf("uli: couldn't create log directory %q: %s", logsdir, err)
+		}
+	}
+	versiondir := path.Join(logsdir, "v"+version)
+	if _, err := os.Stat(versiondir); os.IsNotExist(err) {
+		if err := os.Mkdir(versiondir, 0755); err != nil {
+			log.Fatalf("uli: couldn't create version directory %q: %s", versiondir, err)
+		}
+	}
+	t := time.Now()
+	name := fmt.Sprintf("%d-%d-%d.%d-%d-%d.log", t.Year(), t.Month(), t.Day(),
+		t.Hour(), t.Minute(), t.Second())
+	p := path.Join(versiondir, name)
+	f, err := os.Create(p)
+	if err != nil {
+		log.Fatalf("uli: couldn't create log file %q: %s", p, err)
+	}
+	return f
+}
+
+// Printf logs the message with the request identification at the beginning.
+func Printf(r *http.Request, format string, a ...interface{}) {
+	logger.Printf("%s %s", r.RemoteAddr, fmt.Sprintf(format, a...))
+}
