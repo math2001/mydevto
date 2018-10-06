5a6276754ccace21686e5e6154a2e45316283303
Mathieu PATUREL <australie.p@gmail.com>
2018-10-06 11:26:36 +1000

    fix(index): enable /
    It's gross. Watch out

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"os"
@@ -15,26 +12,6 @@ import (
 	"github.com/math2001/mydevto/services/buildinfos"
 	_ "github.com/math2001/mydevto/services/db"
 )
-
-// This is the homepage, the only page the user is going to load directly
-func index() http.HandlerFunc {
-	githubid := os.Getenv("GITHUBID")
-	if githubid == "" {
-		log.Fatalf("$GITHUBID isn't defined.")
-	}
-	t, err := template.ParseFiles("web/index.tmpl")
-	if err != nil {
-		log.Fatalf("Errored parsing index.html: %s", err)
-	}
-	var b bytes.Buffer
-	if err := t.Execute(&b, map[string]string{"GithubID": githubid}); err != nil {
-		log.Fatalf("Errored executing template: %s", err)
-	}
-	var html = b.String()
-	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, html)
-	}
-}
 
 func main() {
 	log.Println("MyDevTo", buildinfos.V)
--- router/router.go
+++ router/router.go
@@ -1,7 +1,12 @@
 package router
 
 import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/math2001/mydevto/api"
@@ -10,6 +15,27 @@ import (
 	"github.com/math2001/mydevto/services/uli"
 )
 
+// This is the homepage, the only page the user is going to load directly
+// quick and dirty. This is going to be managed by the views in the future.
+func index() http.HandlerFunc {
+	githubid := os.Getenv("GITHUBID")
+	if githubid == "" {
+		log.Fatalf("$GITHUBID isn't defined.")
+	}
+	t, err := template.ParseFiles("web/index.tmpl")
+	if err != nil {
+		log.Fatalf("Errored parsing index.html: %s", err)
+	}
+	var b bytes.Buffer
+	if err := t.Execute(&b, map[string]string{"GithubID": githubid}); err != nil {
+		log.Fatalf("Errored executing template: %s", err)
+	}
+	var html = b.String()
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, html)
+	}
+}
+
 // Router creates the router for the application, delegating it to the
 // controllers
 func Router() *mux.Router {
@@ -17,7 +43,7 @@ func Router() *mux.Router {
 	router.Use(uli.Middleware)
 	router.StrictSlash(true)
 
-	// router.HandleFunc("/", index())
+	router.HandleFunc("/", index())
 	router.PathPrefix("/static").Handler(
 		http.StripPrefix("/static", http.FileServer(http.Dir("web/static"))))
 	manageAPI(router.PathPrefix("/api").Subrouter())
